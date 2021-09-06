cc7fdae72dc60760ab1b5a40cf76abc177293612
McKay Michaelis <mckaykerksiek@gmaillcom>
2021-09-06 09:42:41 -0600

    Initial Commit

---
+++ StockMarket/StockMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMarketApp.swift
+//  StockMarket
+//
+//  Created by McKay Michaelis on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct StockMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
